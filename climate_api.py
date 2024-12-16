644e255955a9507af83be3f86717837372e4166b
Mateus da Cunha Tito <134788822+MTito2@users.noreply.github.com>
2024-12-16 15:21:53 -0300

    :wrench: chore: Realiza pequenas modificações

---
+++ climate_api.py
@@ -0,0 +1,16 @@
+import requests
+
+def get_dados(cidade):
+
+    url = "https://weatherapi-com.p.rapidapi.com/current.json"
+
+    querystring = {"q":cidade}
+
+    headers = {
+        "x-rapidapi-key": "63ea5ecf8bmsh5ea6c4f441a4064p13bff2jsn086fd722fb9f",
+        "x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    return response.json()
+
