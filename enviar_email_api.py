19acd5bb42906bb7b11469009e9fac516940b51c
Mateus da Cunha Tito <134788822+MTito2@users.noreply.github.com>
2024-12-17 08:02:27 -0300

    :sparkles: feat: Habilita o envio de e-mail

---
+++ enviar_email_api.py
@@ -0,0 +1,33 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart 
+from config import EMAIL_GMAIL, SENHA_GMAIL
+
+def enviar_email(destinatario, assunto, corpo):
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    email = EMAIL_GMAIL
+    password = SENHA_GMAIL
+
+    # Criando o objeto do e-mail
+    message = MIMEMultipart()
+    message["From"] = email
+    message["To"] = destinatario
+    message["Subject"] = assunto
+    message.attach(MIMEText(corpo, "plain"))
+
+    try:
+
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()  
+        server.login(email, password)
+
+        server.sendmail(email, destinatario, message.as_string())
+        print("E-mail enviado com sucesso!")
+
+    except Exception as e:
+        print(f"Erro ao enviar e-mail: {e}")
+
+    finally:
+        server.quit()
+
